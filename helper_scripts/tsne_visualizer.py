22a8015691e65cddf30696a48da786434e3c8f43
Yulia Eka Ananta <122988857+yuliaannt@users.noreply.github.com>
2025-01-07 10:34:49 +0900

    Add files via upload

---
+++ helper_scripts/tsne_visualizer.py
@@ -0,0 +1,29 @@
+# from sklearn.manifold import TSNE
+
+# from yellowbrick.contrib.tsne import TSNEVisualizer
+# from yellowbrick.text.tsne import tsne
+from yellowbrick.text import TSNEVisualizer
+import json
+import numpy as np
+from sklearn.cluster import KMeans
+
+# embedded data has fasttext vectors of the sentences
+with open("embedded_data_short.json") as file:
+    data = json.load(file)
+
+X = []
+
+for intent in data["intents"]:
+
+    for pattern in intent["patterns"]:
+        if not (intent["tag"] == "general"):
+            pattern = np.array(pattern)
+            X.append(pattern)
+
+
+clusters = KMeans(n_clusters=5)
+clusters.fit(X)
+
+tsne = TSNEVisualizer()
+tsne.fit(X, ["c{}".format(c) for c in clusters.labels_])
+tsne.poof()
